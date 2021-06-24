8380877baaeb13b9e9b664d2a12c3a286ca8cbcb
GayathriKumar-08 <79191711+GayathriKumar-08@users.noreply.github.com>
2021-06-24 15:58:39 +0530

    Add files via upload

---
+++ JavaScript/Staticcls.js
@@ -0,0 +1,9 @@
+ class Strexample {
+    static searchWord() {
+        var str = "Hello this a static class, welcome to the javascript.";
+        var n = str.includes("javascript");
+        console.log("position " + n);
+    }
+}
+Strexample.searchWord();
+
