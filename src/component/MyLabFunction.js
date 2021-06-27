31fa798a279e74c1688f23b2c83c9137ce224264
GayathriKumar-08 <79191711+GayathriKumar-08@users.noreply.github.com>
2021-06-27 23:50:24 +0530

    Add files via upload

---
+++ src/component/MyLabFunction.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+
+const Labfun=()=>{
+        return (
+            <div>
+                <h3>Iam a child component-Function</h3>
+            </div>
+        )
+}
+export default Labfun;
