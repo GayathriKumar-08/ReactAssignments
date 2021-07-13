4a124a13743450fe36e05ce0b7e1fb94f9f9c47e
GayathriKumar-08 <79191711+GayathriKumar-08@users.noreply.github.com>
2021-07-13 09:51:23 +0530

    Add files via upload

---
+++ Week3/src/services/MessageService.js
@@ -0,0 +1,20 @@
+import { Subject } from "rxjs";
+
+const sub=new Subject();
+
+export const messageService={
+    sendMessage :(message) =>
+
+    sub.next(
+        {
+            text:message
+        }
+    ),
+   
+    getMessage :() =>
+    sub.asObservable(),
+    
+   
+    // clearMessage:() => 
+    // sub.next({})
+}
